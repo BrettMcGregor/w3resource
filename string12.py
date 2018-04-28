43fd432d84f2378ea293a48e7f533e820042266c
Brett McGregor <brett.w.mcgregor@gmail.com>
2018-04-28 10:04:22 +1000

    added comments

--- string12.py
+++ string12.py
@@ -7,16 +7,20 @@ my_string = "How many times is this in this? Exactly this, how many times."
 
 output = []
 
+#remove punctuation from string
 for char in my_string:
     if char not in string.punctuation:
         output.append(char)
+
+# convert string to list
 output = ("".join(output)).split()
 
 my_dict = {}
-
+# create dictionary from list and put word count as value
 for word in output:
     my_dict.update({word: output.count(word)})
 
+# Print a formatted table of results
 print("\n{:10}{}".format("Word", "Count") + "\n" + "-" * 15)
 
 for key, value in my_dict.items():
